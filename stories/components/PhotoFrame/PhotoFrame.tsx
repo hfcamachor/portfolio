d3d0a479f673cc9897c755e1da9b2752681ceab2
Hector Camacho <hectorfernandocr@gmail.com>
2023-11-09 21:53:50 -0500

    update photo frame

--- stories/components/PhotoFrame/PhotoFrame.tsx
+++ stories/components/PhotoFrame/PhotoFrame.tsx
@@ -8,23 +8,53 @@ export interface PhotoFrameProps {
   /**
    * Color of the frame
    */
-  color?: 'blue' | 'pink';
+  color?: "red" | "blue";
 }
 
-export function PhotoFrame({ className, color = 'blue' }: PhotoFrameProps) {
+export function PhotoFrame({ className, color }: PhotoFrameProps) {
   return (
     <div className={className}>
       <svg
         className={styles[`PhotoFrame${color}`]}
         width="100%"
         height="100%"
-        viewBox="0 0 390 525"
+        viewBox="0 0 500 456"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M80.5373 7.61206H77.6257L75.573 9.67694L9.99087 75.6471L7.9552 77.6948V80.5822V509V516H14.9552H301.343H304.276L306.333 513.909L376.96 442.118L378.97 440.075V437.209V14.6121V7.61206H371.97H80.5373Z"
-          stroke-width="5"
+          d="M275.5 456L267 443.475H375.507L420.507 366.475L424.5 380L382.5 456H275.5Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M253.507 3.00009L263.5 16.8675L127.131 16.8675L83.4891 94.6453L77.4998 77L120 3.00009L253.507 3.00009Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M83.5 399L22.9103 293L25.4906 266.475L107.988 407.934L83.5 399Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M356.5 5.99988L342.5 15.4999H373.5L417 91.4999L421 78.9999L379 5.99988H356.5Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M418.718 92.4999L423 82.4999L426 87.9999L421.451 98.1672L418.718 92.4999Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M423.215 99.9997L427.497 89.9997L429.5 93.9999L424.951 104.167L423.215 99.9997Z"
+          fill="#DC4943"
+        />
+        <path
+          d="M426.822 106L431.104 95.9998L432 98.4999L427.451 108.667L426.822 106Z"
+          fill="#DC4943"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M127.887 17.9999L5.7735 229.506L127.887 441.013L372.113 441.013L494.226 229.506L372.113 17.9999L127.887 17.9999ZM375 12.9999L125 12.9999L0 229.506L125 446.013H375L500 229.506L375 12.9999Z"
+          fill="#DC4943"
         />
       </svg>
     </div>
