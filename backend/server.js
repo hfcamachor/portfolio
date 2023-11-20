623bcd3c904db0240b581e00c61715220141cba5
Hector Camacho <hectorfernandocr@gmail.com>
2023-11-20 10:49:28 -0500

    Backend ai api added

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/completions", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.AI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+      max_tokens: 100,
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/generations", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.AI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: req.body.message,
+      n: 1,
+      size: req.body.size,
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("App listening on Port: ", PORT);
+});
